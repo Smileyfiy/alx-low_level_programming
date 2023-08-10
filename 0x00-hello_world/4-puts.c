72ed9f0cf641eae22d79923290a8a66079e68f93
Smileyfiy <johnotieno177@gmail.com>
2023-08-10 11:51:43 -0700

    2

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main-mainfunction
+ *
+ * Return:shlld be returned
+ */
+
+int main(void)
+{
+	puts ("Programming is like building a multilingual puzzle");
+	return(0); 
+
+}

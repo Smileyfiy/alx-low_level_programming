c934afb17d384f07e9cd1b2146715d4f45bda7c2
Smileyfiy <johnotieno177@gmail.com>
2023-08-10 12:40:22 -0700

    k'

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main-mf
+ *
+ * Return:0 always returned
+ */
+
+int main(void)
+{
+	printf("Size of char:%lu byte(s)",size of (char));
+	printf("Size of int:%lu byte(s)",size of (int));
+	printf("Size of long int:%lu Byte(s)",size of (long int));
+	printf("Size of long long int:%lu byte(s)",size of (long long int));
+	printf("Size of float:%lu byte(s)",size of (float));	
+	return (0);
+
+}
